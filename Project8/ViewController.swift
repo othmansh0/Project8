cbcb06e26837f7dc71d8da9e9b5c91fc9d12ce02
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-08-17 18:44:49 +0300

    Add scoreLabel

--- Project8/ViewController.swift
+++ Project8/ViewController.swift
@@ -6,12 +6,33 @@
 //
 
 import UIKit
+//anchors: let us very clearly and descriptively place views relative to each other
 
 class ViewController: UIViewController {
-
+    var cluesLabel: UILabel!
+    var answersLabel: UILabel!
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+ 
     }
 
 
