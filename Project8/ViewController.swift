c6decede77e4574feab753984ebb436f4fde64db
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-08-20 09:41:54 +0300

    Create loadLevel function

--- Project8/ViewController.swift
+++ Project8/ViewController.swift
@@ -22,6 +22,11 @@ class ViewController: UIViewController {
     var scoreLabel: UILabel!
     var letterButtons = [UIButton]()
     
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    var score = 0
+    var level = 1
+    
     override func loadView() {
         view = UIView()
         view.backgroundColor = .white
@@ -68,6 +73,7 @@ class ViewController: UIViewController {
         //create submit button
         let submit = UIButton(type: .system)
         submit.setTitle("SUBMIT", for: .normal)
+        submit.addTarget(self, action: #selector(submitTapped), for: .touchUpInside)
         submit.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(submit)
         
@@ -75,6 +81,7 @@ class ViewController: UIViewController {
         let clear = UIButton(type: .system)
         clear.translatesAutoresizingMaskIntoConstraints = false
         clear.setTitle("CLEAR", for: .normal)
+        clear.addTarget(self, action: #selector(clearTapped), for: .touchUpInside)
         view.addSubview(clear)
         
         //a plain UIView – it host our buttons
@@ -154,7 +161,7 @@ class ViewController: UIViewController {
 
                 // give the button some temporary text so we can see it on-screen
                 letterButton.setTitle("WWW", for: .normal)
-
+                letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
                 // calculate the frame of this button using its column and row
                 let frame = CGRect(x: col * width, y: row * height, width: width, height: height)
                 letterButton.frame = frame
@@ -176,6 +183,71 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadLevel()
+        
+    }
+    
+    @objc func letterTapped(_ sender: UIButton){
+        
+    }
+    
+    @objc func submitTapped(_ sender: UIButton){
+        
+    }
+    
+    @objc func clearTapped(_ sender: UIButton){
+        
+        
+    }
+    
+    func loadLevel(){
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        
+        if let levelFileURL = Bundle.main.url(forResource: "level1", withExtension: "txt"){
+            if let levelContents = try? String(contentsOf: levelFileURL){
+                var lines = levelContents.components(separatedBy: "\n")
+                lines.shuffle()
+                
+                
+                for (index,line) in lines.enumerated(){
+                    let parts = line.components(separatedBy: ": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    
+                    clueString += "\(index + 1). \(clue)\n"
+                   
+                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                    solutionString += "\(solutionWord.count) letters\n"
+                    solutions.append(solutionWord)
+                    
+                    let bits = answer.components(separatedBy: "|")
+                    print(bits)
+                    letterBits += bits
+                  
+                    
+                }
+
+                
+            }
+            
+            
+            
+            
+            
+        }
+        //configure labels and buttons
+        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+        answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        letterBits.shuffle()
+        
+        if letterButtons.count == letterBits.count{
+            for i in 0..<letterButtons.count{
+                letterButtons[i].setTitle(letterBits[i], for: .normal)
+            }
+        }
         
     }
     
